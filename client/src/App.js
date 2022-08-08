d47c709e576a313122ac60fcea115c457941d1da
Joey <joeyhoellerich@gmail.com>
2022-08-08 17:42:28 -0400

    added general styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import WorkoutDayCard from './components/WorkoutDayCard';
+import PastWorkoutTitle from "./imgs/pastworkouts-title.svg"
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <img className = "center" src = {PastWorkoutTitle} alt="past workouts" />
       <WorkoutDayCard />
       <WorkoutDayCard />
     </div>
